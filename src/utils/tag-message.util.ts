9038ccf031d7f3fc1fe51d61f71ca773260691bd
Rodrigo Treviño <ri.trevino96@gmail.com>
2024-02-08 18:37:58 -0600

    feat/Sent message prefix

--- src/utils/tag-message.util.ts
+++ src/utils/tag-message.util.ts
@@ -1,4 +1,4 @@
 import { Contact } from "whatsapp-web.js";
 import { buildTags } from "./build-contact-tag.util";
 
-export const tagMessage = (contacts: Contact[], message: string = "") => `${buildTags(contacts)} ${message}`;
+export const tagMessage = (contacts: Contact[], message: string = "") => `🤖 A21 BOT: ${message} ${buildTags(contacts)}`;
