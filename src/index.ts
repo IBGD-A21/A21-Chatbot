11afd3432ded2217536947b112141ea6aad621fc
Rodrigo Treviño <ri.trevino96@gmail.com>
2024-04-24 19:15:48 -0600

    wweb version patch

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Client, LocalAuth } from "whatsapp-web.js";
 import { clientEvents } from "./events";
 
+const wwebVersion = '2.2412.54';
 const client = new Client({
   authStrategy: new LocalAuth({ clientId: "client-one" }),
+  // locking the wweb version
+  webVersionCache: {
+      type: 'remote',
+      remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+  },
 });
 
 client.initialize();
